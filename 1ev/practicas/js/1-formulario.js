3886cc4bb9cd5c251049f8a6f603904a863f58ac
Roman <romankr1997@gmail.com>
2022-10-03 21:18:22 +0200

    avance en los ejercicios de clase

--- 1ev/practicas/js/1-formulario.js
+++ 1ev/practicas/js/1-formulario.js
@@ -1,22 +1,25 @@
-let contenido = document.getElementById("container");
-function mostrarTexto(){
-    let nombre = document.getElementsByClassName("nombre")[0].value;
-    let email = document.getElementsByClassName("email")[0].value;
-    document.getElementById("parrafo").innerHTML = nombre + " " + email;
-}
-
-
 // Ejercicio 1
+const nombre1 = document.getElementById("nombre1");
+const email1 = document.getElementById("email1");
+const boton1 = document.getElementById("boton1");
+const resultados1 = document.getElementById("resultados1");
 
-let nombre1 = document.getElementById("nombre");
-let email1 = document.getElementById("email");
-let boton1 = document.getElementById("boton1");
-let resultados1 = document.getElementById("resultados1")
-
-function ejercicio1(){
+function ejercicio1 () {
     if(nombre1.value && email1.value){
-        resultados1.innerHTML = `${nombre1.value} tiene la dirección de email ${email1.value}`;
+        resultados1.innerHTML=`${nombre1.value} tiene la dirección de email ${email1.value}`;
     }else{
         resultados1.innerHTML = `Rellena todos los campos`;
+    }
+}
+
+// Ejercicio 2
+const radio2 = document.getElementById("radio2");
+const resultados2 = document.getElementById("resultados2")
+
+function ejercicio2 () {
+    if(radio2.value){
+        resultados2.innerHTML=`CORRECTO`;
+    }else{
+        resultados2.innerHTML = `Rellena el campo`;
     }
 }
