5b3c69e2a8efcaf6aee9c45991e8a2b20f9c7a50
SrGalleto <romankr1997@gmail.com>
2022-10-12 03:19:10 +0200

    efecto sombreado conseguido

--- 1ev/practicas/js/generales.js
+++ 1ev/practicas/js/generales.js
@@ -1,10 +1,4 @@
-// let botones = document.querySelectorAll(".button-nav");
-
-// for(let i = 0; i < botones.length; i++){
-//     botones[i].addEventListener("click", sombrear(i));
-// }
-
 function sombrear(pos){
-    document.querySelector(`#ej${pos}`).classList.remove("box-shadow-1");
-    document.querySelector(`#ej${pos}`).classList.add("box-shadow-1");
+    document.querySelector(`#ej${pos}`).classList.toggle("box-shadow-1");
+    setTimeout(() => { document.querySelector(`#ej${pos}`).classList.toggle("box-shadow-1"); }, 1000);
 }
