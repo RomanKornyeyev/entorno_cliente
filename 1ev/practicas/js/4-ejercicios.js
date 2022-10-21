3248f9a221f5332c82a94533a60c5bebdf8cab83
Roman <romankr1997@gmail.com>
2022-10-21 21:41:01 +0200

    avance ejers

--- 1ev/practicas/js/4-ejercicios.js
+++ 1ev/practicas/js/4-ejercicios.js
@@ -1,4 +1,4 @@
-// Ejercicio 4.1
+// Ejercicio 1
 const boton1 = document.querySelector("#boton1");
 let base1 = document.querySelector("#num1-1");
 let altura1 = document.querySelector("#num1-2");
@@ -8,14 +8,17 @@ boton1.addEventListener("click", ejercicio1);
 
 function ejercicio1 (){
     if(base1.value && altura1.value){
+        baseValor = parseFloat(base1.value);
+        alturaValor = parseFloat(altura1.value);
         resultado1 = 0;
-        resultado1 = parseFloat(base1.value) * 2;
-        resultado1 += parseFloat(altura1.value) * 2;
-        resultados1.innerHTML = `${resultado1.toFixed(2)}`;
+        resultado1 = baseValor * 2;
+        resultado1 += alturaValor * 2;
+        resultados1.innerHTML = `<tr><td><b>Entrada</b></td><td><b>Salida</b></td></tr>
+                                <tr><td>${baseValor.toFixed(2)}, ${alturaValor.toFixed(2)}</td><td>${resultado1.toFixed(2)}</td></tr>`;
     }else resultados1.innerHTML = `Rellena todos los campos`;
 }
 
-// Ejercicio 4.2
+// Ejercicio 2
 const boton2 = document.querySelector("#boton2");
 let numAlumn = document.querySelector("#num2");
 let alumnosInfo = document.querySelector("#alumnosInfo");
@@ -25,7 +28,7 @@ boton2.addEventListener("click", ejercicio2);
 
 function ejercicio2 (){
     if(numAlumn.value) {
-        if(numAlumn.value >= 8 && numAlumn.value <= 20){
+        if(numAlumn.value >= 2 && numAlumn.value <= 20){
             resultados2.innerHTML = `Introduce el nombre y las notas de los ${numAlumn.value} alumnos:`;
             alumnosInfo.innerHTML = ``;
             for (let i = 0; i < numAlumn.value; i++) {
@@ -38,6 +41,7 @@ function ejercicio2 (){
                                             <label class="label-lane"></label> 
                                         </li>`;
             }
+            boton2_1.classList.toggle("display-none");
         }else{
             resultados2.innerHTML = `Introduce un número válido (del 8 al 20)`;
             alumnosInfo.innerHTML = ``;
@@ -60,6 +64,7 @@ function ejercicio2_1 (){
     notas = document.querySelectorAll(".alumnosNotas");
     listaAlumn = [];
     sumaTotal = 0;
+    tabla2_1.innerHTML = ``; //limpieza
     tabla2_1.innerHTML += `<tr><td><b>Alumno</b></td><td><b>Nota</b></td></tr>`;
     for (let i = 0; i < alumnos.length; i++) {
         listaAlumn[i] = [alumnos[i].value, parseInt(notas[i].value)];
@@ -70,4 +75,33 @@ function ejercicio2_1 (){
                         <tr><td><b>MEDIA:</b></td><td>${sumaTotal/alumnos.length}</td></tr>`;   
 }
 
-//Ejercicio 4.3
+//Ejercicio 3
+const boton3 = document.querySelector("#boton3");
+let strings = document.querySelectorAll('.strings');
+let resultados3 = document.querySelector("#resultados3");
+
+
+
+boton3.addEventListener("click", ejercicio3);
+
+function ejercicio3(){
+    let p1 = strings[0].value.toLowerCase();
+    let letras2 = strings[1].value.toLowerCase().split("");
+   
+    let iguales = true;
+    let cont = 0;
+
+    letras2.forEach(element => {
+        if(!p1.includes(element)){
+            iguales=false;
+        }
+    });
+
+    if(iguales == true) resultados3.innerHTML = `Resultado: ${true}`;
+    else resultados3.innerHTML = `Resultado: ${true}`;
+
+
+    // if(strings[0].value.toLowerCase().includes(strings[1].value.toLowerCase())){
+    //     resultados3.innerHTML = `Resultado: ${true}`;
+    // }else resultados3.innerHTML = `Resultado: ${false}` ;
+}
