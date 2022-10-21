e8b3d69640120b36def50ee7bf96c3a27390426c
SrGalleto <romankr1997@gmail.com>
2022-10-22 00:32:16 +0200

    reorganizacion y ejer 4-1 terminado

--- 1ev/practicas/js/4.1-ejercicios.js
+++ 1ev/practicas/js/4.1-ejercicios.js
@@ -1,18 +1,22 @@
 // Ejercicio 1
+//definimos variables
 const boton1 = document.querySelector("#boton1");
 let base1 = document.querySelector("#num1-1");
 let altura1 = document.querySelector("#num1-2");
 let resultados1 = document.querySelector("#resultados1");
 
+//añadimos un un evento al hacer click en el botón
 boton1.addEventListener("click", ejercicio1);
 
+//función que se ejecuta al hacer click en el botón
 function ejercicio1 (){
-    if(base1.value && altura1.value){
+    if(base1.value && altura1.value){ //si todos los campos están llenos
         baseValor = parseFloat(base1.value);
         alturaValor = parseFloat(altura1.value);
         resultado1 = 0;
         resultado1 = baseValor * 2;
         resultado1 += alturaValor * 2;
+        //hacemos los cálculos y printeamos las filas y columnas de la tabla en el HTML, con los resultados
         resultados1.innerHTML = `<tr><td><b>Entrada</b></td><td><b>Salida</b></td></tr>
                                 <tr><td>${baseValor.toFixed(2)}, ${alturaValor.toFixed(2)}</td><td>${resultado1.toFixed(2)}</td></tr>`;
     }else resultados1.innerHTML = `Rellena todos los campos`;
@@ -28,7 +32,7 @@ boton2.addEventListener("click", ejercicio2);
 
 function ejercicio2 (){
     if(numAlumn.value) {
-        if(numAlumn.value >= 2 && numAlumn.value <= 20){
+        if(numAlumn.value >= 8 && numAlumn.value <= 20){
             resultados2.innerHTML = `Introduce el nombre y las notas de los ${numAlumn.value} alumnos:`;
             alumnosInfo.innerHTML = ``;
             for (let i = 0; i < numAlumn.value; i++) {
@@ -80,28 +84,21 @@ const boton3 = document.querySelector("#boton3");
 let strings = document.querySelectorAll('.strings');
 let resultados3 = document.querySelector("#resultados3");
 
-
-
 boton3.addEventListener("click", ejercicio3);
 
 function ejercicio3(){
-    let p1 = strings[0].value.toLowerCase();
-    let letras2 = strings[1].value.toLowerCase().split("");
-   
-    let iguales = true;
-    let cont = 0;
+    if(strings[0].value && strings[1].value){
+        let p1 = strings[0].value.toLowerCase();
+        let letras2 = strings[1].value.toLowerCase().split("");
+        let iguales = true;
 
-    letras2.forEach(element => {
-        if(!p1.includes(element)){
-            iguales=false;
-        }
-    });
+        letras2.forEach(element => {
+            if(!p1.includes(element)){
+                iguales=false;
+            }
+        });
 
-    if(iguales == true) resultados3.innerHTML = `Resultado: ${true}`;
-    else resultados3.innerHTML = `Resultado: ${true}`;
-
-
-    // if(strings[0].value.toLowerCase().includes(strings[1].value.toLowerCase())){
-    //     resultados3.innerHTML = `Resultado: ${true}`;
-    // }else resultados3.innerHTML = `Resultado: ${false}` ;
+        if(iguales) resultados3.innerHTML = `Resultado: ${true}`;
+        else resultados3.innerHTML = `Resultado: ${false}`;
+    }else resultados3.innerHTML = `Rellena todos los campos`;
 }
