9f24c24d6809102a97fd715d41ee6e98c148898d
SrGalleto <romankr1997@gmail.com>
2022-10-24 03:07:08 +0200

    ejercicio 3.2 terminado

---
+++ 1ev/practicas/js/3.2-ejercicios.js
@@ -0,0 +1,17 @@
+//Ejer 1
+let fechaActual = new Date;
+let annioActual = fechaActual.getFullYear(); //averiguamos el año actual
+
+//definimos el fin del año
+let finAnnio = new Date(annioActual, 11, 31, 23, 59, 59);
+//variable para pasar ms a días
+let diaMilisegundos = 1000 * 60 * 60 * 24; 
+
+//calculamos los días restantes
+let diasRestantes = Math.ceil((finAnnio.getTime() - fechaActual.getTime()) / (diaMilisegundos));
+
+// --- pintamos los resultados con un botón
+let resultados = document.querySelector("#resultados1");
+let boton = document.querySelector("#boton1");
+boton.addEventListener("click", ejercicio1);
+function ejercicio1(){resultados.innerHTML = `Quedan ${diasRestantes} para el fin de este año.`;}
