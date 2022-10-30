c6c7c2cc352dd54fed8efc6dabfd171d72590bfa
SrGalleto <romankr1997@gmail.com>
2022-10-30 13:45:15 +0100

    3.3 mejorado

--- 1ev/practicas/3-ejercicios/3.3-ejercicios/3.3_ventanas.js
+++ 1ev/practicas/3-ejercicios/3.3-ejercicios/3.3_ventanas.js
@@ -1,27 +1,32 @@
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
+const res = document.querySelector("#resultados1");
 
 abrir.addEventListener("click", abrirVentana);
 
-function abrirVentana(){
+function abrirVentana() {
 	let url = document.querySelector('#url').value;
 	let vs = window.open(url, "ventana1", "width=720,height=480");
 
 	cerrar.addEventListener("click", cerrarVentana);
-	function cerrarVentana(){if(!vs.closed) vs.close();}
+	function cerrarVentana() { if (!vs.closed) vs.close(); }
+	// extra (CONTADOR)
+	function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
+
+	async function contar() {
+		for (i = 10; i > 0; i--) {
+			if (!vs.closed){
+				res.innerHTML = `La ventana secundaria se cerrará en: ${i} segundos`;
+				await sleep(1000);
+			}else{
+				break;
+			}
+		}
+		res.innerHTML = 'Ventana cerrada';
+	}
 	//extra (contador)
 	contar();
-	
-	setTimeout(() => {if(!vs.closed) vs.close();}, 10000);
+
+	setTimeout(() => { if (!vs.closed) vs.close(); }, 10000);
 }
 
-// extra (CONTADOR)
-function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms));}
-
-async function contar() {
-    for (i = 10; i > 0; i--) {
-        console.log(`La ventana se cerrará en: ${i} segundos`);
-        await sleep(1000);
-    }
-    console.log('Ventana cerrada');
-}
