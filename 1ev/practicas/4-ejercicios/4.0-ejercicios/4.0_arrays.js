e1a437420cd8b01900ef3550705080148e01464f
Roman <romankr1997@gmail.com>
2022-11-08 21:45:03 +0100

    ejer 4.0 a medias

--- 1ev/practicas/4-ejercicios/4.0-ejercicios/4.0_arrays.js
+++ 1ev/practicas/4-ejercicios/4.0-ejercicios/4.0_arrays.js
@@ -1,1 +1,17 @@
-//ejer 1
+const sevilla = document.querySelector("#sevilla");
+const huelva = document.querySelector("#huelva");
+const cadiz = document.querySelector("#cadiz");
+
+let listaSevilla = [];
+let listaHuelva = [];
+let listaCadiz = [];
+
+sevilla.addEventListener("click", llenarLista);
+huelva.addEventListener("click", llenarLista);
+cadiz.addEventListener("click", llenarLista);
+
+function  llenarLista(){
+    while (res!=0) {
+        
+    }
+}
