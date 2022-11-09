6dfa356ba7d23ce033904b073df5e75b351c5555
SrGalleto <romankr1997@gmail.com>
2022-11-09 04:07:25 +0100

    ejer 4.0 terminado

--- 1ev/practicas/4-ejercicios/4.0-ejercicios/4.0_arrays.js
+++ 1ev/practicas/4-ejercicios/4.0-ejercicios/4.0_arrays.js
@@ -49,11 +49,13 @@ function llenarLista(){
     }else avisos.innerHTML = `Rellena todos los campos para añadir el empleado`;
 
     //apartado A
-    longitudTot();
+    imprimirLong();
     //apartado B
     imprimirMedias();
     //apartado C
     imprimirOrdenados();
+    //apartado D
+    imprimirMayMen();
 }
 
 function introducirEmple(arr, delegacion){
@@ -74,11 +76,11 @@ function introducirEmple(arr, delegacion){
 
 //apartado A
 let resA = document.querySelector("#res-a");
-function longitudTot(){
+function imprimirLong(){
     resA.innerHTML = `<b>a) Número de empleados:</b><br>
                         SEVILLA: ${listaSevilla.length}<br>
                         HUELVA: ${listaHuelva.length}<br>
-                        CADIZ: ${listaCadiz.length}`;
+                        CÁDIZ: ${listaCadiz.length}`;
 }
 
 //apartado B
@@ -106,22 +108,51 @@ function media(arr){
 //apartado C
 let resC = document.querySelector("#res-c");
 function imprimirOrdenados(){
-    let suelSev = ordenar(listaSevilla);
+    let suelHuel = ordenar(listaHuelva, "to-up");
+    let suelSev = ordenar(listaSevilla, "to-down");
+    let suelCad = ordenar(listaCadiz, "to-down");
 
-    resC.innerHTML = `<b>b) ORDENACIONES:</b><br>
-                        HUELVA (de menor a mayor): ${suelSev}`;
+    resC.innerHTML = `<b>c) ORDENACIONES DE SUELDOS:</b><br>
+                        HUELVA (de menor a mayor): ${suelHuel}<br>
+                        SEVILLA (de mayor a menor): ${suelSev}<br>
+                        CÁDIZ (de mayor a menor): ${suelCad}`;
 }
 
-function ordenar(arr){
+function ordenar(arr, type){
     let nums = [];
-    for (var i = 0; i < arr.length; i++) {
+    for (var i = 0; i < arr.length; i++)
         nums.push(parseInt(arr[i][1]));
-    }
+
     //ordenamos el array (sin volcado)
-    nums.sort((a, b) => b+a);
-    cadena = "";
-    for (let i = 0; i < nums.length; i++) {
-        cadena += nums[i]+", ";
+    switch (type) {
+        case "to-up":nums.sort((a, b) => a-b);break; //de menor a mayor
+        case "to-down":nums.sort((a, b) => b-a);break; //de mayor a menor
+        default:break;
     }
+    cadena = "";
+    //cargamos el array ordenado en una cadena
+    for (let i = 0; i < nums.length; i++)
+        cadena += nums[i]+", ";
+    //devolvemos la cadena
     return cadena;
+}
+
+//apartado D
+let resD = document.querySelector("#res-d");
+function imprimirMayMen(){
+    let nums = [];
+    for (var i = 0; i < listaSevilla.length; i++)
+        nums.push(parseInt(listaSevilla[i][1]));
+    for (var i = 0; i < listaHuelva.length; i++)
+        nums.push(parseInt(listaHuelva[i][1]));
+    for (var i = 0; i < listaCadiz.length; i++)
+        nums.push(parseInt(listaCadiz[i][1]));
+
+    //ordenamos de mayor a menor y cogemos los extremos posteriormente (así obtenemos el max y min de todo)
+        nums.sort((a, b) => a-b);
+   
+
+    resD.innerHTML = `<b>c) MÍNIMO Y MÁXIMO (DE LAS 3 DELEGACIONES):</b><br>
+                        MÍNIMO: ${nums[0]}<br>
+                        MÁXIMO: ${nums[nums.length-1]}`;
 }
--- 1ev/practicas/4-ejercicios/4.0-ejercicios/generales.js
+++
@@ -1,4 +0,0 @@
-function sombrear(pos){
-    document.querySelector(`#ej${pos}`).classList.toggle("box-shadow-1");
-    setTimeout(() => { document.querySelector(`#ej${pos}`).classList.toggle("box-shadow-1"); }, 1000);
-}
