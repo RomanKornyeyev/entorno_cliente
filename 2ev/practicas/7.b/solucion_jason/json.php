c134969c670fcd2a8abe1ec1ad0e1c7c06734498
SrGalleto <romankr1997@gmail.com>
2023-02-18 14:21:40 +0100

    algunas pequeñas cosas updateadas

---
+++ 2ev/practicas/7.b/solucion_jason/json.php
@@ -0,0 +1,15 @@
+<?php
+
+header('Content-Type: application/json;');
+
+$conn = new PDO('mysql:host=localhost;dbname=dwes', 'byeejasonn', '1234', [
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+]);
+
+$stmt = $conn->prepare("SELECT * FROM dwec_server WHERE name = :name");
+$stmt->bindParam(":name", $_POST['name']);
+
+$stmt->execute();
+$users = $stmt->fetch();
+
+echo json_encode($users, JSON_UNESCAPED_SLASHES);
